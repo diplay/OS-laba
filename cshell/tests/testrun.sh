8ddf4de67320248b5b74cbd38d4999c2efc1e8ea
diplay <diplaySuper@gmail.com>
2013-12-06 23:12:47 +0400

    начал пилить тесты на csh

--- cshell/tests/testrun.sh
+++ cshell/tests/testrun.sh
@@ -1,21 +1,19 @@
+#!/bin/csh
 echo "Номер теста,Требования,Строка запуска,Объекты ФС,Код возврата,Сообщение,Объекты ФС,Вердикт"
-TESTS=`ls`
-TIMESTAMP=`date +%s`
-while [ -e $TIMESTAMP ]
-do
+set TESTS=`ls`
+set TIMESTAMP=`date +%s`
+while ( -e $TIMESTAMP )
 	let TIMESTAMP++
-done
+end
 mkdir $TIMESTAMP
 cp ../append-to-end.sh append-to-end.sh
 chmod +x append-to-end.sh
 cd $TIMESTAMP
-for x in $TESTS
-do
-	if [ `expr match $x "test[0-9]*\.sh"` -ne 0 ]
-	then
+foreach x ($TESTS)
+	if ( `expr match $x "test[0-9]*\.sh"` != 0 ) then
 		chmod +x ../$x
 		../$x
-	fi
-done
+	endif
+end
 cd ..
 rm -rf $TIMESTAMP
