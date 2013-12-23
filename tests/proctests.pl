e78d6c11d91d82115ace121f6a20306c8eacf0d9
Levon <diplaySuper@gmail.com>
2013-12-23 22:38:50 +0400

    improving perl script

--- tests/proctests.pl
+++ tests/proctests.pl
@@ -5,8 +5,13 @@ printf "#!/bin/csh\n";
 while ($line = <FILE_SH>)
 {
 	$line =~ s/\$\?/\$status/;
-	if($line =~ m/\s*then$/)
+	if($line =~ m/\s*(then|do)$/)
 	{}
+	elsif($line =~ m/(\s*)for ([A-Za-z0-9_]*) in (\$[A-Za-z0-9_]*).*/)
+	{
+		printf "$1foreach $2 ($3)\n";
+	}
+	#elsif($line =~ m/(\s*)while ()/)
 	elsif($line =~ m/(\s*if[^\n]*)\n$/)
 	{
 		$line = "$1 ) then\n";
@@ -14,6 +19,10 @@ while ($line = <FILE_SH>)
 		$line =~ s/if/if \(/;
 		$line =~ s/=/==/;
 		$line =~ s/-eq/==/;
+		$line =~ s/-gt/>/;
+		$line =~ s/-lt/</;
+		$line =~ s/-ge/>=/;
+		$line =~ s/-le/<=/;
 		printf $line;
 	}
 	elsif($line =~ m/(\s*)([A-Za-z0-9_.]*=.*)/)
@@ -21,9 +30,13 @@ while ($line = <FILE_SH>)
 		$line = "$1set $2\n";
 		printf "$line";
 	}
-	elsif($line =~ m/^fi$/)
+	elsif($line =~ m/(\s*)fi\s*$/)
 	{
-		printf "endif\n";
+		printf "$1endif\n";
+	}
+	elsif($line =~ m/(\s*)done\s*$/)
+	{
+		printf "$1end\n";
 	}
 	else
 	{
